bc71913d1b072e51e0f86c94db5453b305115192
deepanshi-mirg123 <deepanshi.mirg@knoldus.com>
2024-01-04 23:51:11 +0530

    uncomment-code

--- projects/proj-2/src/app/app.module.ts
+++ projects/proj-2/src/app/app.module.ts
@@ -1,27 +1,3 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { UserComponent } from './app.component';
-// import { ActiveUsersComponent} from './components/active-user/active-user.component';
-// import { InactiveUsersComponent } from './components/inactive-user/inactive-user.component';
-// import { UsersService } from './services/user.services';
-
-// @NgModule({
-//   imports: [
-//     BrowserModule,
-    
-
-//   ],
-//   declarations: [
-//     UserComponent,
-//     ActiveUsersComponent,
-//     InactiveUsersComponent
-//   ],
- 
-//   providers: [UsersService],
-//   bootstrap: [UserComponent]
-// })
-// export class AppModule { }
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
